5724bdf8a9646fca5b1550abdb68abe0e65f068e
Lin Yuan <yuanlin.dalian@gmail.com>
2016-11-01 13:06:08 -0400

    Çrawl data for jobtong WIP.

--- index.js
+++ index.js
@@ -30,10 +30,25 @@ function visitPage(url, callback) {
         // Parse the document body
         var $ = cheerio.load(body);
         searchForWord($);
-        setTimeout(callback, 1000);
+        // setTimeout(callback, 1000);
     });
 }
 function searchForWord($) {
-    var companyName = $('h1', '.header').text();
-    console.log('companyName', companyName);
+    var json = {};
+    json.companyName = $('h1', '.header').text();
+
+    var companyInfo = $('span.tag', 'div.tags');
+    json.companyAddress = $(companyInfo[0]).text();
+    json.companyEmployeeCount = $(companyInfo[1]).text();
+    json.companyType = $(companyInfo[2]).text();
+    json.companyIndustry = $(companyInfo[3]).text();
+
+    var parentCompanyInfo = $('p', '.sidebar');
+    json.parentCompanyName = $(parentCompanyInfo[0]).text();
+    json.parentCompanyWebsite = $(parentCompanyInfo[1]).text();
+    json.parentCompanyAddress = $(parentCompanyInfo[2]).text();
+    json.parentCompanyInfo = cheerio.text(parentCompanyInfo);
+
+    json.companyIntroduction = $('div', 'div.introduce').text();
+
 }
