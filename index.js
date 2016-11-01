036569b22983f3712ff404887203ef52b8cdcfc3
Lin Yuan <yuanlin.dalian@gmail.com>
2016-11-01 11:48:39 -0400

    Çrawl data for jobi WIP.

--- index.js
+++ index.js
@@ -1,43 +1,26 @@
 import request from 'request';
 import cheerio from 'cheerio';
-import URL from 'url-parse';
 import fs from 'fs';
 
-var START_URL = "http://bj.58.com/sou/?key=%E7%AE%A1%E5%9F%B9%E7%94%9F&button=%E6%90%9C%C2%A0%E7%B4%A2";
-var SEARCH_WORD = "stemming";
-var MAX_PAGES_TO_VISIT = 10000;
+let index = 99;
+var base = '000000';
 
-var pagesVisited = {};
-var numPagesVisited = 0;
-var pagesToVisit = [];
-var url = new URL(START_URL);
-var baseUrl = url.protocol + "//" + url.hostname;
-
-pagesToVisit.push(START_URL);
 crawl();
 
 function crawl() {
-    if (numPagesVisited >= MAX_PAGES_TO_VISIT) {
-        console.log("Reached max limit of number of pages to visit.");
-        return;
-    }
-    var nextPage = pagesToVisit.pop();
-    if (nextPage in pagesVisited) {
-        // We've already visited this page, so repeat the crawl
-        crawl();
-    } else {
-        // New page we haven't visited
-        visitPage(nextPage, crawl);
-    }
+    var nextPage = getNextUrl();
+    visitPage(nextPage, crawl);
+}
+
+function getNextUrl() {
+    index++;
+    return 'http://www.jobif.com/jobprovider_' +
+        base.slice(0, index.toString().length) + index + '.htm';
 }
 
 function visitPage(url, callback) {
-    // Add page to our set
-    pagesVisited[url] = true;
-    numPagesVisited++;
 
     // Make the request
-    console.log("Visiting page " + url);
     request(url, function (error, response, body) {
         // Check status code (200 is HTTP OK)
         if (error || !response || response.statusCode !== 200) {
@@ -46,19 +29,11 @@ function visitPage(url, callback) {
         }
         // Parse the document body
         var $ = cheerio.load(body);
-        var relativeLinks = $("a[href^='/']");
-        relativeLinks.each(function () {
-            pagesToVisit.push(baseUrl + $(this).attr('href'));
-
-        });
-        searchForWord($, SEARCH_WORD);
-        crawl();
+        searchForWord($);
+        callback();
     });
 }
 function searchForWord($) {
-    var wordsRegex = /海外|管培生|美国|澳大利亚|管理培训生|英国|留美|海归/g;
-    var bodyText = $('html > body').text().toLowerCase();
-    if (wordsRegex.test(bodyText)) {
-        fs.appendFileSync('info.txt', bodyText);
-    }
+    var companyName = $('h2', 'span.title').text();
+    console.log(companyName);
 }
